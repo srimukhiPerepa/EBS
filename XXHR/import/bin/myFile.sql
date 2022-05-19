33cbc3b2a2c944f343805ce1928690e24a803702
srimukhiPerepa <105828046+srimukhiPerepa@users.noreply.github.com>
2022-05-19 11:20:07 +0530

    Update myFile.sql

--- XXHR/import/bin/myFile.sql
+++ XXHR/import/bin/myFile.sql
@@ -1,4 +1,4 @@
-CREATE TABLE sqlTableContents (
+CREATE TABLE newTableContents (
     PersonID int,
     LastName varchar(255),
     FirstName varchar(255),
@@ -6,6 +6,8 @@ CREATE TABLE sqlTableContents (
     City varchar(255)
 );
 
-INSERT INTO sqlTableContents VALUES( 123, 'lastName', 'FirstName', 'Address', 'City');
+INSERT INTO newTableContents VALUES( 123, 'lastName', 'FirstName', 'Address', 'City');
 
-SELECT * FROM sqlTableContents;
+SELECT * FROM newTableContents;
+
+DROP TABLE newTableContents;
