adf8602c0bb323fbc5f942243a990edfa11d1778
srimukhiPerepa <105828046+srimukhiPerepa@users.noreply.github.com>
2022-05-19 05:40:41 +0530

    Add files via upload

---
+++ XXHR/import/bin/insertValues.sql
@@ -0,0 +1,1 @@
+insert into Persons values(124, "perepa", "srimukhi", "lig-526", "Hyderabad");
---
+++ XXHR/import/bin/myFile.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+insert into Persons value( 123, "lastName", "FirstName", "Address", "City");
+
+select * from Persons;
