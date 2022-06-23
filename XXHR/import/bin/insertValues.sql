3357f2e3c95a8327bc3fda1aeef2fc59709324e1
srimukhiPerepa <105828046+srimukhiPerepa@users.noreply.github.com>
2022-06-23 18:03:20 +0530

    removed commented lines

--- XXHR/import/bin/insertValues.sql
+++ XXHR/import/bin/insertValues.sql
@@ -2,7 +2,7 @@ CREATE TABLE country_table1 (
     Address varchar(255),
     City varchar(255)
 );
-/* altering values xyz abc opq1234567891011112*/
+
 ALTER TABLE country_table1
 DROP COLUMN City;
 
