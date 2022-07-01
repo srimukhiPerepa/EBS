ffdb84fa56ea6f36afafb71b3fd1c056b2625aec
srimukhiPerepa <105828046+srimukhiPerepa@users.noreply.github.com>
2022-07-01 15:09:22 +0530

    insertValues.sql modified

--- XXHR/import/bin/insertValues.sql
+++ XXHR/import/bin/insertValues.sql
@@ -2,7 +2,7 @@ CREATE TABLE country_table1 (
     Address varchar(255),
     City varchar(255)
 );
-/* new commented line  1*/
+/* new commented line1*/
 ALTER TABLE country_table1
 DROP COLUMN City;
 
