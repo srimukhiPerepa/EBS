bb619f59c1eba4c29ff54144531b599f4631e57a
srimukhiPerepa <105828046+srimukhiPerepa@users.noreply.github.com>
2022-07-07 11:35:36 +0530

    Update myFile.sql

--- XXHR/import/bin/myFile.sql
+++ XXHR/import/bin/myFile.sql
@@ -4,6 +4,7 @@ CREATE TABLE newTableContents (
     FirstName varchar(255),
     Address varchar(255),
     City varchar(255)
+    
 );
 
 INSERT INTO newTableContents VALUES( 123, 'lastName', 'FirstName', 'Address', 'City');
