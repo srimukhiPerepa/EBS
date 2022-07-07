c761bb7d88930bf246bbec890f3b886edec8b205
srimukhiPerepa <105828046+srimukhiPerepa@users.noreply.github.com>
2022-07-07 11:30:20 +0530

    Update myFile.sql

--- XXHR/import/bin/myFile.sql
+++ XXHR/import/bin/myFile.sql
@@ -9,4 +9,5 @@ CREATE TABLE newTableContents (
 INSERT INTO newTableContents VALUES( 123, 'lastName', 'FirstName', 'Address', 'City');
 SELECT * FROM newTableContents;
 
+
 DROP TABLE newTableContents;
