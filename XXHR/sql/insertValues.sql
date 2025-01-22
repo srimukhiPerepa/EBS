b26ed3276db634cc4ff51a92eb771b7b4544e7e0
Flexagon <105828046+srimukhiPerepa@users.noreply.github.com>
2025-01-22 18:46:56 +0530

    1st revision

--- XXHR/sql/insertValues.sql
+++ XXHR/sql/insertValues.sql
@@ -1,3 +1,3 @@
-insert into country_table1 values (1234, 'srimukhi', 'perepa', 'DNFSDFD', 'HYD');
+insert into country_table1 values (1234, 'srimukhi1', 'perepa', 'DNFSDFD', 'HYD');
 insert into country_table1 values(12345, 'hari', 'PE', 'ADD1', 'RAJ');
 insert into country_table1 values(12345, 'rashi', 'kahna', 'address', 'VIZAG')
